6a5753f0805f8496d7aa959abf5d606addc6cb6a
ckdgus2938 <ckdgus2938@gmail.com>
2018-08-03 17:11:41 +0900

    20180803

--- SWExpert_2382/SWExpert_2382/main.cpp
+++ SWExpert_2382/SWExpert_2382/main.cpp
@@ -1,122 +1,138 @@
+
 #include <iostream>
 #include <vector>
-
-
 using namespace std;
-
 int result;
 int N,M,K;
-//. (상: 1, 하: 2, 좌: 3, 우: 4)
-int moveN[4] = { -1 , 1, 0,0 };
-int moveM[4] = { 0,  0 , -1,1};
+int nx[4] = {-1,0 ,1,0};
+int ny[4] = { 0,-1,0,1};
 
-vector<pair< pair<int,int>, pair<int,int> > >::iterator vi;
-
-int recursion( vector<pair< pair<int,int>, pair<int,int> > > tempV ,int misang ,int hour){
+// 벽에 부딪히면 미생물 수 1/2   + 방향 반대
+// 미생물 끼리 부딪혔을 때 미생물수는 합해지고 방향은 많았던 미생물 방향
+class Virus{
+public:
+    int n,m,c,v;
+    int maxC;
     
-    //cout<< "hour" << hour<<endl;
-    if( hour == M ){
-        return misang;
+    Virus(){
     }
-    else{
-    // move
-        for(vi = tempV.begin();vi != tempV.end(); vi++){
+    ~Virus(){
+    }
+    void init( int tn, int tm, int tc, int tv){
+        n=tn ;
+        m=tm;
+        c=tc;
+        v=tv;
+        maxC =0 ;
+    }
+    void meetMedicine(){
+        c = c /2 ;
+        v = (v +2 ) % 4;
+    }
+    bool checkMedicine(){
+        if( n == 0 || n == N-1 || m == 0 || m == N-1 ){
+            // medicine
+            return true;
+        }
+        else
+            return false;
+    }
+};
+vector< Virus> vec;
+
+void solve(){
+    for( int time = 0; time < M; time++){
+        //cout<< time<<"------------"<<endl;
+        //vector<int> tarr[101][101];
         
-            int nextN = vi->first.first + moveN[ vi->second.second -1];
-            int nextM = vi->first.second + moveM[ vi->second.second -1];
+        int tempArr[101][101][2] ={0,};
         
-            if( nextN == 0 || nextM == 0 || nextN == N-1 || nextM == N-1){
-    
-              
-                vi->second.first /= 2;
-                
-                if( vi->second.second == 1){
-                    vi->second.second = 2;
+        for( int i =0 ; i< vec.size() ; i++ ){
+            if( vec[i].c != 0 ){
+                vec[i].n += nx[ vec[i].v];
+                vec[i].m += ny[ vec[i].v];
+                //tarr[vec[i].n][vec[i].m].push_back( i);
+                if( vec[i].checkMedicine()){
+                    vec[i].meetMedicine();
                 }
-                else if( vi->second.second == 2){
-                    vi->second.second = 1;
+                tempArr[vec[i].n][vec[i].m][1] += vec[i].c;
+                if(  tempArr[vec[i].n][vec[i].m][0] < vec[i].c){
+                    tempArr[vec[i].n][vec[i].m][0] = vec[i].c;
+                    
                 }
-                else if( vi->second.second == 3){
-                    vi->second.second = 4;
-                }
-                else if( vi->second.second == 4){
-                    vi->second.second = 3;
-                }
-                
-                vi->first.first = nextN;
-                vi->first.second = nextM;
+            }
+        }
+        for( int i=0; i< vec.size(); i++){
+            if( vec[i].c < tempArr[vec[i].n][vec[i].m][0] ){
+                vec[i].c = 0;
             }
             else{
-                vi->first.first = nextN;
-                vi->first.second = nextM;
+                vec[i].c = tempArr[vec[i].n][vec[i].m][1];
             }
         }
-    
-        vector<pair< pair<int,int>, pair<int,int> > > memoV;
-        // merge
-        vector<pair< pair<int,int>, pair<int,int> > >::iterator vj;
-        int pC = 0;
-        for(vi = tempV.begin();vi != tempV.end(); vi++){
-            int temp =vi->second.first;
-            int bigV = vi->second.first ;
-            int totalV = vi->second.first;
-            int vV  = vi->second.second;
-            int chk = true;
-            for( vj = tempV.begin() ; vj!= tempV.end(); vj++){
-                if( vi == vj ){
-                    continue;
-                }
-                else{
-                    if( vi->first.first == vj->first.first && vi->first.second == vj->first.second){
-                        totalV += vj->second.first;
-                        if( bigV < vj->second.first){
-                            bigV = vj->second.first;
-                            vV = vj->second.second;
+        
+        /*
+        for(int i=0; i<N ; i++){
+            for( int j=0; j<N ;j++){
+                if( tarr[i][j].size() > 1){
+                    //cout<< i<<" "<< j <<endl;
+                    int biggest = vec[tarr[i][j][0]].c;
+                    int bigI = 0;
+                    for( int k =1; k< tarr[i][j].size(); k++){
+                        if(  vec[tarr[i][j][k]].c > biggest){
+                            biggest = vec[tarr[i][j][k]].c;
+                            bigI = k;
                         }
-                        
                     }
+                    int sum = 0;
+                    int tarrLen =  tarr[i][j].size();
+                    for( int k =0; k<tarrLen; k++){
+                        if( k!= bigI ) {
+                            sum += vec[tarr[i][j][k]].c;
+                            vec[tarr[i][j][k]].c =0 ;
+                        }
+                    }
+                    vec[tarr[i][j][bigI]].c += sum ;
                 }
             }
-            if( temp == bigV){
-                pC += totalV;
-                memoV.push_back( make_pair( make_pair( vi->first.first, vi->first.second) , make_pair( totalV,vV)));
-            }
         }
-        return recursion( memoV, pC, hour +1);
+        */
+        
     }
-
- 
+    
 }
 
 int main(int argc, const char * argv[]) {
-    int case_total;
-    cin>> case_total;
-    
-    vector<pair< pair<int,int>, pair<int,int> > >vec;
-    
-    
-    for(int casei = 1 ; casei <= case_total; casei++){
-
+    int T;
+    cin>> T;
+    for(int t= 1; t<= T ; t++){
+        result =0 ;
         vec.clear();
-        
-        
-        cin>> N>>M>>K;
-        int tm,tn, tc, tv ;
-        int misang = 0 ;
-        for( int i =0 ; i < K; i++){
-            cin>> tn>> tm>> tc>> tv;
-            misang+= tc;
-            vec.push_back(make_pair(make_pair(tn,tm), make_pair(tc,tv)));
+        cin>> N>> M>>K;
+        for(int k=0; k< K ; k++){
+            int n,m,c,v;
+            cin>> n>>m>>c>>v;
+            Virus tempVirus;
             
-            //arr[tn][tm] = tc;
-            //varr[tn][tm] = tv;
+            if( v==  1)
+                tempVirus.init( n,m,c,0);
+            else if( v== 2)
+                tempVirus.init( n,m,c,2);
+            else if( v== 3)
+                tempVirus.init( n,m,c,1);
+            else
+                tempVirus.init( n,m,c,3);
+            
+            vec.push_back(tempVirus);
         }
         
+        solve();
+        for( int i =0 ; i< vec.size() ; i++ ){
+            result += vec[i].c;
+        }
         
-        
-        cout<<"#"<<casei<<" "<< recursion( vec, misang, 0)<<endl;
+        cout<<"#"<<t<<" "<< result<<endl;
     }
-    
     
     
     return 0;
